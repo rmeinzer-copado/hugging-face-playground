683141b8597ae04fb117d1640ab3227547fa26ff
Ryan Meinzer <rmeinzer@copado.com>
2024-02-27 14:28:36 -0800

    adding ToDo for BERT with ultimately NER

---
+++ BERT.py
@@ -0,0 +1,3 @@
+# ToDo - adapt pipeline() to use BERT pretrained model for inference with "content-classification" then try NER
+# https://huggingface.co/docs/transformers/quicktour#use-another-model-and-tokenizer-in-the-pipeline
+# https://huggingface.co/docs/transformers/task_summary#text-classification
